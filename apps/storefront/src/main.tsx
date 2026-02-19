999ebd52a9d53ab8ab8f18ad1823f4d9e6f38640
Shaun Nesbitt <shaun@smartpay.co.nz>
2026-02-20 12:16:19 +1300

    minor

--- apps/storefront/src/main.tsx
+++ apps/storefront/src/main.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/storefront">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </QueryClientProvider>
